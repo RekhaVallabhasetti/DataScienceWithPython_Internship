f4851d52deeb8acbe127da50bf2c7ea5c4237ae3
RekhaVallabhasetti <124146790+RekhaVallabhasetti@users.noreply.github.com>
2024-04-23 13:05:44 +0530

    Add files via upload

---
+++ Main Flow Tak-1.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+list1 = [1, 12, 30, 56, 90]
+
+list1.append(61)
+print("List after adding element:", list1)
+
+list1.remove(30)
+print("List after removing element:", list1)
+
+
+list1[0] = 11
+print("List after modifying element:", list1)
+
+
+
+# In[8]:
+
+
+my_dict = {'a': 12, 'b': 20, 'c': 33}
+
+my_dict['d'] = 47
+print("Dictionary after adding element:", my_dict)
+
+
+del my_dict['b']
+print("Dictionary after removing element:", my_dict)
+
+my_dict['a'] = 100
+print("Dictionary after modifying value:", my_dict)
+
+
+
+# In[9]:
+
+
+my_set = {1, 2, 3, 4, 5}
+
+my_set.add(6)
+print("Set after adding element:", my_set)
+
+my_set.remove(3)
+print("Set after removing element:", my_set)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
